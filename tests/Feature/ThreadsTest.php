a3ea9cf7b4372b3efe71a9640f8c379c56fc1472
Salvatore Mulas <salvo@salvoblog.ch>
2017-11-12 16:41:14 +0100

    Extended testcases with Replies

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -10,21 +10,34 @@ class ThreadsTest extends TestCase
 {
     use DatabaseMigrations;
 
+    public function setUp()
+    {
+        parent::setUp();
+        $this->thread = factory('App\Thread')->create();
+    }
+
     /** @test */
     public function a_user_can_browse_threads()
     {
-        $thread = factory('App\Thread')->create();
-
-        $response = $this->get('/threads');
-        $response->assertSee($thread->title);
+        $this->get('/threads')
+            ->assertSee($this->thread->title);
     }
 
     /** @test */
     public function a_user_can_browse_a_single_thread()
     {
-        $thread = factory('App\Thread')->create();
-
-        $response = $this->get('/threads/' . $thread->id);
-        $response->assertSee($thread->title);
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($this->thread->title);
     }
+
+    /** @test */
+    public function a_user_can_read_replies_associated_to_a_thread()
+    {
+        $reply = factory('App\Reply')
+            ->create(['thread_id' => $this->thread->id]);
+
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($reply->body);
+    }
+
 }
